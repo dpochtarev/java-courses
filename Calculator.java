e972040167d818d78e8656b7411a3f9653aa5a0b
dpochtarev <ts4dmitry@yandex.ru>
2015-03-19 16:02:22 +0300

    Добавлены операции и работа с результатом

--- Calculator.java
+++ Calculator.java
@@ -1,15 +1,15 @@
 /**
-	Класс реализует калькулятор.
+	пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ.
 */
 public class Calculator {
 	/**
-		Результат вычисления.
+		пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ.
 	*/
 	private int result;
 	
 	/**
-		Суммируем аргументы.
-		@param params Аргументы суммирования.
+		пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ.
+		@param params пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ.
 	*/
 	public void add(int ... params) {
 		for (Integer param : params) {
@@ -17,16 +17,47 @@ public class Calculator {
 		}	
 	}
 
+    public void sub(int ... params) {
+        int lenght = params.length;
+        for(Integer param : params) {
+                if(lenght==params.length) {
+                    this.result = param;
+                    lenght--;
+                } else this.result -=param;
+            }
+        }
+
+
+    public void mult(int ... params) {
+        int length = params.length;
+        for (Integer param : params) {
+            if(length==params.length){
+                this.result = param;
+                length--;
+            }else this.result *= param;
+        }
+    }
+
+    public void div(int ... params) {
+        int length = params.length;
+        for (Integer param : params) {
+            if(length==params.length){
+                this.result = param;
+                length--;
+            }else this.result /= param;
+        }
+    }
+
 	/**
-		Получить результат.
-		@return результат вычисления.
+		пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ.
+		@return пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ.
 	*/
 	public int getResult() {
 		return this.result;
 	}
 
 	/**
-		Очистить результат вычисления.
+		пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ.
 	*/
 	public void cleanResult() {
 		this.result = 0;
--- InteractRunner.java
+++ InteractRunner.java
@@ -1,28 +1,49 @@
 import java.util.Scanner;
 
 /**
-	Класс для запуска калькулятора. Поддерживает ввод пользователя.
-*/
+ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ. пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ.
+ */
 public class InteractRunner {
-	
-	public static void main(String[] arg) {
-		Scanner reader = new Scanner(System.in);
-		try {
-			Calculator calc = new Calculator();
-			String exit = "no";
-			while (!exit.equals("yes")) {
-				System.out.println("Enter first arg : ");
-				String first = reader.next();
-				System.out.println("Enter second arg : ");
-				String second = reader.next();
-				calc.add(Integer.valueOf(first), Integer.valueOf(second));
-				System.out.println("Result : " + calc.getResult());
-				calc.cleanResult();
-				System.out.println("Exit : yes/no ");
-				exit = reader.next();
-			}
-		} finally {
-			reader.close();
-		}
-	}
+
+    public static void main(String[] arg) {
+        Scanner reader = new Scanner(System.in);
+        try {
+            Calculator calc = new Calculator();
+            String exit = "no";
+            String first = null;
+            boolean result = false;
+            while (!exit.equals("y")) {
+                if(result) {
+                    first = String.valueOf(calc.getResult());
+                    System.out.println(first);
+                }
+                else {
+                    System.out.println("Enter first arg : ");
+                    first = reader.next();
+                    calc.cleanResult();
+                }
+
+                System.out.println("Enter Operation : ");
+                String operation = reader.next();
+                System.out.println("Enter second arg : ");
+                String second = reader.next();
+
+                if("+".equals(operation)) calc.add(Integer.valueOf(first), Integer.valueOf(second));
+                if("-".equals(operation)) calc.sub(Integer.valueOf(first), Integer.valueOf(second));
+                if("*".equals(operation)) calc.mult(Integer.valueOf(first), Integer.valueOf(second));
+                if("/".equals(operation)) calc.div(Integer.valueOf(first), Integer.valueOf(second));
+
+                System.out.println("Result : " + calc.getResult());
+                System.out.println("Continue with result ? : y/n");
+                if(reader.next().equals("n"))  {
+                    System.out.println("Exit : y/n ");
+                    exit = reader.next();
+                    calc.cleanResult();
+                }
+                else result=true;
+            }
+        } finally {
+            reader.close();
+        }
+    }
 }
